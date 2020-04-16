4e5857d329de9035512688beb1adc6fe4748eeab
Nifemi <deyeminifemi@gmail.com>
2020-04-17 00:27:12 +0100

    Removed barId from token fields and disabled password required

--- Models/Token.js
+++ Models/Token.js
@@ -7,21 +7,15 @@ const tokenSchema = new Schema({
         ref: 'Bar'
     },
 
-    barId: {
-        type: String,
-        required: true
-    },
-
     password: {
         type: String,
-        required: true
     },
 
     date: {
         type: Date,
         default: Date.now
     }
-})
+});
 
 const Token = mongoose.model('Token', tokenSchema);
 
