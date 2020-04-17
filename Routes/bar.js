8d27757999fa6ad255db178f888a5ce8382fb6ba
Nifemi <deyeminifemi@gmail.com>
2020-04-17 15:38:33 +0100

    Made email work with created app password

--- Routes/bar.js
+++ Routes/bar.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const nodemailer = require('nodemailer');
 require('dotenv/config');
-const config = require('config');
-
 
 const Bar = require('../Models/Bar');
 const Token = require('../Models/Token');
@@ -70,8 +68,7 @@ router.route('/')
                 await user.save();
 
                 const smtpTransport = nodemailer.createTransport({
-                    service: "IMAP",
-                    host: process.env.SMTP_HOST,
+                    service: 'gmail',
                     auth: {
                         user: process.env.SMTP_USER,
                         pass: process.env.SMTP_PASSWORD
@@ -80,11 +77,17 @@ router.route('/')
 
                 const mailOptions = {
                     to: bar.email,
-                    from: config.get('SMTP_USER'),
+                    from: process.env.SMTP_USER,
                     subject: 'Your Bar ID',
                     html: `
                         <h1>Congrats you have successfully signed up</h1>
-                        <h3>Kindly go to <a href="https://naijabarrescue.netlify.app/pub/create-password?id=${bar._id}">this</a> link to get started</h3>
+                        <h3>
+                            Thank you for joining for the Naija Bar Rescue Initiative, now your consumers will be able to see your bar on the platform and buy vouchers.
+                            Follow <a href="https://naijabarrescue.netlify.app/pub/create-password?id=${bar._id}">this</a> link to sign in to your profile where you can see a list of vouchers purchased at your bar.
+                        </h3>
+                        <h3>
+                            For more information, please contact us - support@naijabarrescue.com or call 09062820138
+                        </h3>
                     `
                 };
 
@@ -92,11 +95,10 @@ router.route('/')
                     if (err) {
                         return res.status(500).send({message: err.message, success: false});
                     }
-                });
-
-                res.status(200).json({
-                    message: 'A verification email has been sent to ' + bar.email + '.',
-                    success: true
+                    res.status(200).json({
+                        message: 'A verification email has been sent to ' + bar.email + '.',
+                        success: true
+                    });
                 });
             } catch (err) {
                 res.status(500).json({message: err + 'Error', success: false})
