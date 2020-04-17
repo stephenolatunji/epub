ee9696b7141693092f457b13a3fa867676b352ee
Nifemi <deyeminifemi@gmail.com>
2020-04-18 00:54:08 +0100

    Made create order post route work with new models

--- Routes/order.js
+++ Routes/order.js
@@ -2,50 +2,66 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/oauth');
 
-
 const Order = require('../Models/Order');
-const User = require('../Models/User');
-const Bar = require('../Models/Bar');
+const Voucher = require('../Models/Voucher');
 
 router.route('/')
 
-    
+
     // @route       POST/User
     // @desc        Make new order
     // access       Private
 
-    .post( auth, async (req, res) => {
+    // .post(auth, async (req, res) => {
+    .post(async (req, res) => {
 
-        
-        try{
-            const order = new Order({ 
-                name: user.name,
-                user: req.user.id,
-                bar: req.body.bar,
-                amount: req.body.amount,
-                quantity: req.body.quantity,
-                total: req.body.total
+        const {reference, userId, barId, vouchers} = req.body;
+
+        //Verify reference using https://api.paystack.co/transaction/verify/refId
+
+
+        try {
+            const vouchersMapped = vouchers.map(({price, quantity}) => ({
+                price,
+                quantity,
+                userId,
+                barId,
+                total: quantity * price
+            }));
+
+            const vouchersDb = await Voucher.create(vouchersMapped);
+
+
+            const order = new Order({
+                userId,
+                barId,
+                vouchers: vouchersDb,
+                total: vouchersMapped.reduce((currentTotal, {total}) => currentTotal + total, 0)
             });
 
-           const newOrder = await order.save();
-           res.json(newOrder);
+            res.json({
+                success: true,
+                order
+            })
 
-        }catch(err){
-            res.status(500).json(err + 'Error')
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: 'Error: ' + err
+            })
         }
 
     })
 
-    .get( auth, async (req, res) => {
-         
-        try{
+    .get(auth, async (req, res) => {
 
-            const order = await Order.find({ user: req.user.id})
+        try {
+
+            const order = await Order.find({user: req.user.id});
             res.json(order)
-        }
-        catch(err){
+        } catch (err) {
             res.status(500).json(err + 'Error')
         }
     });
 
-    module.exports = router;
+module.exports = router;
