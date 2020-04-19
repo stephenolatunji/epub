385c146a0c4d3314c1f9db5222094ed3adfef01b
Nifemi <deyeminifemi@gmail.com>
2020-04-19 17:18:28 +0100

    Removed console.log

--- Routes/bar.js
+++ Routes/bar.js
@@ -44,7 +44,6 @@ router.route('/')
     .post(
         parser.single('image'),
         async (req, res) => {
-            console.log(req.body)
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res.status(400).json({errors: errors.array()})
